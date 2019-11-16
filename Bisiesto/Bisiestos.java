a10dd7e04309ad3bd51cb4cd9cb630de8ae6feb9
Daniel Cabezas Recio <daniel.cabezas.recio@gmail.com>
2019-11-16 11:35:34 +0100

    nueva forma de bisiesto

--- Bisiesto/Bisiestos.java
+++ Bisiesto/Bisiestos.java
@@ -2,13 +2,15 @@
 
 import java.security.InvalidParameterException; //para la excepción invalid parameter exception
 //https://dosc.oracle.com/javase/7/docs/api/java/security/InvalidParameterException.html
+import java.util.GregorianCalendar; //para el calendario gregoriano 
 
 public class Bisiestos {
 	
 	public boolean esBisiesto(int año) throws InvalidParameterException{
+		GregorianCalendar calendar = new GregorianCalendar();
 		boolean bisiesto=false;
-		if(año>0) { //para evaluar los que son válidos 
-			if((año % 4 ==0) && ((año %100 !=0) || (año % 400 ==0 )))
+		if(año>0) {
+			if (calendar.isLeapYear(año))
 				bisiesto=true;
 		}else {
 			throw new InvalidParameterException("Parámetro no válido,introduzca valores positivos");
@@ -16,3 +18,4 @@ public class Bisiestos {
 		return bisiesto;
 	}
 }
+
