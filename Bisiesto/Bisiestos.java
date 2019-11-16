b22641371230c2e705bfd8e74ba8b939c335d7fe
Daniel Cabezas Recio <daniel.cabezas.recio@gmail.com>
2019-11-16 11:28:00 +0100

    añadido bisiestos

--- Bisiesto/Bisiestos.java
+++ Bisiesto/Bisiestos.java
@@ -15,17 +15,4 @@ public class Bisiestos {
 		}
 		return bisiesto;
 	}
-
-	public static void main(String[] args) {  //para probar el algoritmo anterior 
-		// TODO Auto-generated method stub
-		Bisiestos prueba=new Bisiestos();
-		System.out.println(prueba.esBisiesto(2020)); //este es bisiesto 
-		System.out.println(prueba.esBisiesto(2000)); //este es bisiesto
-		System.out.println(prueba.esBisiesto(2400)); //este es bisiesto
-		System.out.println(prueba.esBisiesto(1600)); //este es bisiesto
-		System.out.println(prueba.esBisiesto(1900)); //este no es bisiesto
-		System.out.println(prueba.esBisiesto(2100)); //este no es bisiesto
-		System.out.println(prueba.esBisiesto(2300)); //este no es bisiesto
-		System.out.println(prueba.esBisiesto(-25)); //este no es válido 
-	}
 }
