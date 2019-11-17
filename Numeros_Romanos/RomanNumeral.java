07be9b5b586a7a14c68574ca52de3bf36705de7f
Daniel Cabezas Recio <daniel.cabezas.recio@gmail.com>
2019-11-17 11:56:50 +0100

    primera versión romanos

---
+++ Numeros_Romanos/RomanNumeral.java
@@ -0,0 +1,64 @@
+//package ejercicio_numerosromanos;
+
+import java.util.HashMap;
+import java.security.InvalidParameterException;
+
+public class RomanNumeral {
+  static HashMap<Character, Integer> map;
+
+  public RomanNumeral() {
+    map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+  }
+
+  public int obtenerValorRomanoNumerico(char ch) {
+    if (map.containsKey(ch)) {
+      return map.get(ch);
+    }
+    else {
+      throw new InvalidParameterException("El número romano contiene caracteres no válidas ");
+    }
+  }
+  
+  public boolean comprobarRomano(String roman){
+
+     return !roman.contains("IIII") && !roman.contains("XXX") && !roman.contains("CCC")
+	&& !roman.contains("MMM") && !roman.contains("VV") && !roman.contains("LL")
+	&& !roman.contains("DD");
+  }
+
+  public int convierte(String s) throws InvalidParameterException {
+    if(s==null || s.isEmpty() || !comprobarRomano(s)) {
+	throw new InvalidParameterException("El string romano está vacío o no cumple los parámetros");
+    }
+    else {
+      int index = s.length() - 1;
+      int result = obtenerValorRomanoNumerico(s.charAt(index));
+
+      for (int i = index - 1; i >= 0; i--) {
+        if (obtenerValorRomanoNumerico(s.charAt(i)) >= obtenerValorRomanoNumerico(s.charAt(i + 1))) {
+          result = result + obtenerValorRomanoNumerico(s.charAt(i));
+        }
+        else {
+          result = result - obtenerValorRomanoNumerico(s.charAt(i));
+        }
+      }
+      return result;
+    }
+  }
+  
+  public static void main(String[] args){
+	  System.out.println(new RomanNumeral().convierte("XCIX")); //99
+	  System.out.println(new RomanNumeral().convierte("XIV")); //14
+	  System.out.println(new RomanNumeral().convierte("MMCM")); //2900
+	  System.out.println(new RomanNumeral().convierte("IIIIIII")); //no válido
+	  System.out.println(new RomanNumeral().convierte("ZKP")); //caracteres incorrectos 
+	  System.out.println(new RomanNumeral().convierte("")); //vacío
+	}
+}
