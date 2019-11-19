3d68ae806d0d85a004fe27f84b3984b37bbfebe2
Daniel Cabezas Recio <daniel.cabezas.recio@gmail.com>
2019-11-19 11:18:39 +0100

    corregidas algunas cosas

--- BlackFriday/DescuentoBlackFriday.java
+++ BlackFriday/DescuentoBlackFriday.java
@@ -1,27 +1,26 @@
-package Ejercicio_blackfriday;
+//package ejercicio_blackfriday;
 
 import java.security.InvalidParameterException;
 import java.util.Calendar;
 
 public class DescuentoBlackFriday {
 
-	public static double PrecioFinal(double PrecioOriginal, double Descuento)throws InvalidParameterException {
+	public static double PrecioFinal(double precioOriginal, double porcentajeDescuento)throws InvalidParameterException {
 		Calendar calendar = Calendar.getInstance();
 		int day = calendar.get(Calendar.DATE);
 		int month = calendar.get(Calendar.MONTH) + 1; //Note: +1 the month for current month
 		//CASO ERROR
-		if (PrecioOriginal <= 0) {
+		if (precioOriginal <= 0) {
 			throw new InvalidParameterException("Precio no válido");
 		}
 		
-		if(((100 - Descuento)/100) < 0 || ((100 - Descuento)/100) > 1) {
+		if(((100 - porcentajeDescuento)/100) < 0 || ((100 - porcentajeDescuento)/100) > 1) {
 			throw new InvalidParameterException("Descuento ha de ser entre el 0 y 100");
 		}
 
 		if (day == 29 && month == 11) {
-			return PrecioOriginal * ((100 - Descuento)/100);
+			return precioOriginal * ((100 - porcentajeDescuento)/100);
 		}
-		return PrecioOriginal;
+		return precioOriginal;
 	}
 }
-
